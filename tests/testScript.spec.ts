5ed503f8d78c7319eb5c0cbf9df7bd7c5109117b
akarisan <joa.tomoda@gmail.com>
2023-12-17 11:37:47 +0900

    fix: formated

--- tests/testScript.spec.ts
+++ tests/testScript.spec.ts
@@ -11,28 +11,28 @@ const keywords = {
 };
 
 
-  try {
-    const csvFilePath = "./tests/KeywordDrivenTest.csv";
-    const testSteps = parseCSV(csvFilePath);
+try {
+  const csvFilePath = "./tests/KeywordDrivenTest.csv";
+  const testSteps = parseCSV(csvFilePath);
 
-    for (const [testStep, values] of Object.entries(testSteps)) {
-      test(testStep, async ({ page }) => {
-        for (const [keyword, args] of values) {
-          await executeKeyword(page, keyword, args);
-        }
-        await page.screenshot({ path: "./screenshot.png" });
-      });
-    }
-
-    async function executeKeyword(page: any, keyword:string, args:string) {
-      if (typeof keywords[keyword] === "function") {
-        await keywords[keyword](page, args);
-      } else {
-        console.error(`${keyword}は定義されていません`);
+  for (const [testStep, values] of Object.entries(testSteps)) {
+    test(testStep, async ({ page }) => {
+      for (const [keyword, args] of values) {
+        await executeKeyword(page, keyword, args);
       }
-    }
-  } catch (error) {
-    console.error(error);
-  } finally {
-    console.log("テスト終了");
+      await page.screenshot({ path: "./screenshot.png" });
+    });
   }
+
+  async function executeKeyword(page: any, keyword: string, args: string) {
+    if (typeof keywords[keyword] === "function") {
+      await keywords[keyword](page, args);
+    } else {
+      console.error(`${keyword}は定義されていません`);
+    }
+  }
+} catch (error) {
+  console.error(error);
+} finally {
+  console.log("テスト終了");
+}
