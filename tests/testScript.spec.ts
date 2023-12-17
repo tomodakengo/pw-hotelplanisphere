2a095e5c859f5c64777fc64f63a9110453d6c13d
akarisan <joa.tomoda@gmail.com>
2023-12-17 11:16:34 +0900

    fix: TC列をkeyにしてテストケースを実行できるように修正しました

--- tests/testScript.spec.ts
+++ tests/testScript.spec.ts
@@ -6,15 +6,25 @@ const keywords = {
   URLを開く: km.URLを開く,
   Pathを確認する: km.Pathを確認する,
   メニューをクリックする: km.メニューをクリックする,
+  ボタンをクリックする: km.ボタンをクリックする,
   プラン表示を確認する: km.プラン表示を確認する,
 };
 
-test("Keyword Driven Test", async ({ page }) => {
+
   try {
     const csvFilePath = "./tests/KeywordDrivenTest.csv";
     const testSteps = parseCSV(csvFilePath);
 
-    for (const [keyword, args] of testSteps) {
+    for (const [testStep, values] of Object.entries(testSteps)) {
+      test(testStep, async ({ page }) => {
+        for (const [keyword, args] of values) {
+          await executeKeyword(page, keyword, args);
+        }
+        await page.screenshot({ path: "./screenshot.png" });
+      });
+    }
+
+    async function executeKeyword(page: any, keyword:string, args:string) {
       if (typeof keywords[keyword] === "function") {
         await keywords[keyword](page, args);
       } else {
@@ -24,6 +34,5 @@ test("Keyword Driven Test", async ({ page }) => {
   } catch (error) {
     console.error(error);
   } finally {
-    await page.screenshot({ path: "./screenshot.png" });
+    console.log("テスト終了");
   }
-});
